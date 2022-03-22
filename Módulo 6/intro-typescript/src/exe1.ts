bf8a29e748e8bd935c990ff6ee229ed244bfe976
dnlMatos <georginedanilo@gmail..com>
2022-03-22 20:23:17 -0300

    intro-typescript

---
+++ Módulo 6/intro-typescript/src/exe1.ts
@@ -0,0 +1,14 @@
+function checaTriangulo(a: number, b: number, c: number): string {
+  if (a !== b && b !== c) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
+}
+
+checaTriangulo(2, 5, 3);
+
+console.log("exe1 chamado");
+

7b9d094b789e5e1d0ee107bdc71cd76dc58d043d
Danilo Matos <60492862+dnlMatos@users.noreply.github.com>
2022-07-22 23:42:05 -0300

    Add files via upload

--- Módulo 7/APIFILMES/tmdb/src/components/descricao/Descricao.jsx
+++ Módulo 7/APIFILMES/tmdb/src/components/descricao/Descricao.jsx
@@ -1,8 +1,36 @@
-import React from "react";
-import { Button } from "./buttonStyled";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import { Button } from "./buttonStyled";
+import axios from "axios";
+import { BASE_GENRE } from "../../constants/base_url";
+import { Context } from "../../context/context";
+import { useContext } from "react";
 
 export default function Descricao() {
+  const [generos, setGeneros] = useState([]);
+  const { filtros, setFiltros } = useContext(Context);
+
+  const generosFiltro = [];
+  useEffect(() => {
+    chamaGeneros();  
+  }, []);
+
+  const chamaGeneros = async () => {
+    const result = await axios.get(`${BASE_GENRE}`);
+    setGeneros(result.data.genres);
+  };
+
+  const filtrosEscolhidos = (id) => {
+    if (generosFiltro.includes(id)) {
+      const indice = generosFiltro.indexOf(id)
+      generosFiltro.splice(indice, 1)
+    } else {
+      generosFiltro.push(id)
+    }
+    // setFiltros(generosFiltro)
+  };
+
+
   return (
     <div className="container-fluid descricao">
       <div className="container pt-5 pb-5">
@@ -10,15 +38,20 @@ export default function Descricao() {
           Milhões de filmes, séries e pessoas <br /> para descobrir. Explore já.
         </h2>
         <p className="mt-4 mb-2 text-center">Filtre por:</p>
-        <div className="container d-flex">
-          <Button className="btn mx-auto">Ação</Button>
-          <Button className="btn mx-auto">Crime</Button>
-          <Button className="btn mx-auto">Documentário</Button>
-          <Button className="btn mx-auto">Comédia</Button>
-          <Button className="btn mx-auto">Aventura</Button>
-          <Button className="btn mx-auto">Terror</Button>
-          <Button className="btn mx-auto">Suspense</Button>
-          <Button className="btn mx-auto">Infantil</Button>
+        <div className="container ">
+          <div className="d-flex flex-wrap gap-3 justify-content-center">
+            {generos.map((genero) => {
+              return (
+                <Button
+                  key={genero.id}
+                  className="btn gap-3"
+                  onClick={() => filtrosEscolhidos(genero.id)}
+                >
+                  {genero.name}
+                </Button>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>

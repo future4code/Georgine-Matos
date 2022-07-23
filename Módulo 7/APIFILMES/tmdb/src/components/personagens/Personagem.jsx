3ac677b1afa2a3a46e555ae7936a47287ce9c9b3
dnlMatos <georginedanilo@gmail.com>
2022-07-23 00:06:58 -0300

    as

---
+++ Módulo 7/APIFILMES/tmdb/src/components/personagens/Personagem.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { pegaPersonagens } from "../../requests/request";
+
+export default function Personagem() {
+  const params = useParams();
+  const novoParams = Number(params.id.replace(":", ""));
+  const [personagem, setPersonagem] = useState([]);
+
+  useEffect(() => {
+    chamaPersonagens(novoParams);
+  }, []);
+
+  const chamaPersonagens = async (idFilme) => {
+    const personagens = await pegaPersonagens(idFilme);
+    personagens?.crew && setPersonagem(personagens.crew);
+  };
+
+  return (
+    <div className="container d-flex flex-wrap gap-4 p-0">
+      {personagem
+        .filter((per) => {
+          return per.job == "Producer" || per.job == "Director" || per.job == "Characters";
+        })
+        .map((res, index) => {
+          return (
+            <div key={index}>
+              <h6>{res.name}</h6>
+              <p>{res.job}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
